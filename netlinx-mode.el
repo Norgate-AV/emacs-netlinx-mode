1d75650386d50e2761597386ef12a50031727d3f
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2025-11-23 22:39:44 +0000

    feat: update highlights

--- netlinx-mode.el
+++ netlinx-mode.el
@@ -21,6 +21,24 @@
 (require 'treesit)
 ;; (require 'netlinx-mode-font-lock)
 
+;; Define custom face for device variables (dv/vdv)
+(defface netlinx-device-variable-face
+  '((t :inherit font-lock-variable-name-face :slant italic))
+  "Face for NetLinx device variables (identifiers starting with dv or vdv)."
+  :group 'netlinx-mode)
+
+;; Define custom face for user-defined types
+(defface netlinx-user-type-face
+  '((t :inherit font-lock-type-face :slant italic))
+  "Face for NetLinx user-defined types (type_identifier)."
+  :group 'netlinx-mode)
+
+;; Define custom face for string quotes
+(defface netlinx-string-quote-face
+  '((t :inherit font-lock-operator-face :weight bold))
+  "Face for NetLinx string quote characters."
+  :group 'netlinx-mode)
+
 ;; Tell Emacs where to download the NetLinx grammar from
 (add-to-list 'treesit-language-source-alist
              '(netlinx "https://github.com/Norgate-AV/tree-sitter-netlinx"))
@@ -33,6 +51,12 @@
 ;; Define syntax highlighting rules
 (defvar netlinx-mode--font-lock-settings
   (treesit-font-lock-rules
+   ;; Comments
+   :language 'netlinx
+   :feature 'comment
+   '((comment) @font-lock-comment-face)
+
+   ;; Keywords
    :language 'netlinx
    :feature 'keyword
    '([(break_keyword)
@@ -47,13 +71,10 @@
       (while_keyword)
       (select_keyword)
       (active_keyword)
-
       (struct_keyword)
       (structure_keyword)
-
       (program_name_keyword)
       (module_name_keyword)
-
       (define_device_keyword)
       (define_constant_keyword)
       (define_type_keyword)
@@ -71,15 +92,12 @@
       (define_program_keyword)
       (define_call_keyword)
       (define_module_keyword)
-
       (push_keyword)
       (release_keyword)
       (hold_keyword)
       (repeat_keyword)
-
       (on_keyword)
       (off_keyword)
-
       (online_keyword)
       (offline_keyword)
       (onerror_keyword)
@@ -87,7 +105,6 @@
       (command_keyword)
       (awake_keyword)
       (standby_keyword)
-
       (send_level_keyword)
       (send_string_keyword)
       (send_command_keyword)
@@ -96,43 +113,178 @@
       (create_multi_buffer_keyword)
       (call_keyword)
       (system_call_keyword)
-
       (devchan_on_keyword)
       (devchan_off_keyword)
       (devchan_to_keyword)
       (devchan_min_to_keyword)
       (devchan_total_off_keyword)
       (devchan_pulse_keyword)
-
       (wait_keyword)
       (wait_until_keyword)
       (cancel_wait_keyword)
       (cancel_wait_until_keyword)
       (cancel_all_wait_keyword)
       (cancel_all_wait_until_keyword)
-
       (button_event_keyword)
       (channel_event_keyword)
       (data_event_keyword)
       (level_event_keyword)
       (timeline_event_keyword)
       (custom_event_keyword)
-
       (band)
       (bor)
       (bxor)
       (bnot)
       (lshift)
       (rshift)
-
       (and)
       (or)
       (not)
       (xor)] @font-lock-keyword-face)
 
-    :language 'netlinx
-    :feature 'string
-    '((string_literal) @font-lock-string-face)))
+   ;; Qualifiers
+   :language 'netlinx
+   :feature 'keyword
+   '([(constant_keyword)
+      (volatile_keyword)
+      (non_volatile_keyword)
+      (persistent_keyword)] @font-lock-keyword-face)
+
+   ;; Storage classes
+   :language 'netlinx
+   :feature 'keyword
+   '([(local_var_keyword)
+      (stack_var_keyword)] @font-lock-keyword-face)
+
+   ;; Preprocessor
+   :language 'netlinx
+   :feature 'preprocessor
+   '([(preproc_include_keyword)
+      (preproc_define_keyword)
+      (preproc_warn_keyword)
+      (preproc_disable_warning_keyword)
+      (preproc_if_defined_keyword)
+      (preproc_if_not_defined_keyword)
+      (preproc_else_keyword)
+      (preproc_end_if_keyword)] @font-lock-preprocessor-face)
+
+   ;; Preproc arguments
+   :language 'netlinx
+   :feature 'preprocessor
+   '((preproc_arg) @font-lock-constant-face)
+
+   ;; Operators
+   :language 'netlinx
+   :feature 'operator
+   '(["=" "+" "-" "*" "/" "%" ">" "<" "&" "|" "^" "~"
+      "&&" "||" "==" "!=" "<=" ">=" "<<" ">>" "++" "--" "<>"
+      (range_operator)] @font-lock-operator-face)
+
+   ;; Negation operators
+   :language 'netlinx
+   :feature 'operator
+   '(["!" (not)] @font-lock-negation-char-face)
+
+   ;; Punctuation brackets
+   :language 'netlinx
+   :feature 'bracket
+   '(["(" ")" "{" "}" "[" "]"] @font-lock-bracket-face)
+
+   ;; Punctuation delimiters
+   :language 'netlinx
+   :feature 'delimiter
+   '(["." ";" "," ":"] @font-lock-delimiter-face)
+
+   ;; Literals - strings
+   :language 'netlinx
+   :feature 'string
+   '((string_literal) @font-lock-string-face
+     (escape_sequence) @font-lock-escape-face
+     "\"" @netlinx-string-quote-face
+     ((string_literal) @font-lock-builtin-face
+      (:match "^'/.*/'$" @font-lock-builtin-face)))
+
+   ;; Literals - numbers
+   :language 'netlinx
+   :feature 'number
+   '((number_literal) @font-lock-number-face
+     (device_literal) @font-lock-number-face)
+
+   ;; Literals - booleans
+   :language 'netlinx
+   :feature 'boolean
+   '([(true) (false)] @font-lock-constant-face)
+
+   ;; Types
+   :language 'netlinx
+   :feature 'type
+   '((type_identifier) @netlinx-user-type-face
+     (primitive_type) @font-lock-type-face
+     (structured_type) @font-lock-type-face
+     (system_type) @font-lock-type-face)
+
+   ;; Properties
+   :language 'netlinx
+   :feature 'property
+   '((field_identifier) @font-lock-property-use-face)
+
+   ;; Functions - builtin
+   :language 'netlinx
+   :feature 'function
+   '((call_expression
+      function: (system_function) @font-lock-builtin-face))
+
+   ;; Functions - calls
+   :language 'netlinx
+   :feature 'function
+   '((call_expression
+      function: (identifier) @font-lock-function-call-face))
+
+   ;; Functions - definitions
+   :language 'netlinx
+   :feature 'function
+   '((function_definition
+      name: (identifier) @font-lock-function-name-face))
+
+   ;; Functions - declarations
+   :language 'netlinx
+   :feature 'function
+   '((function_declaration
+      name: (identifier) @font-lock-function-name-face))
+
+   ;; Variables
+   :language 'netlinx
+   :feature 'variable
+   '((system_variable) @font-lock-variable-name-face
+     (compiler_variable) @font-lock-builtin-face)
+
+   ;; Constants - system
+   :language 'netlinx
+   :feature 'constant
+   '((system_constant) @font-lock-constant-face)
+
+   ;; Device variables (dv/vdv prefix) - must be before ALL_CAPS and catch-all
+   :language 'netlinx
+   :feature 'variable
+   '(((identifier) @netlinx-device-variable-face
+      (:match "^v?dv[a-zA-Z_][a-zA-Z\\d_]*$" @netlinx-device-variable-face)))
+
+   ;; Constants - ALL_CAPS identifiers
+   :language 'netlinx
+   :feature 'constant
+   '(((identifier) @font-lock-constant-face
+      (:match "^[A-Z][A-Z\\d_]*$" @font-lock-constant-face)))
+
+   ;; Error nodes
+   :language 'netlinx
+   :feature 'error
+   '((ERROR) @font-lock-warning-face
+     (MISSING) @font-lock-warning-face)
+
+   ;; Identifiers (catch-all for regular variables, must be last)
+   :language 'netlinx
+   :feature 'variable
+   '((identifier) @font-lock-variable-name-face)))
 
 ;; Create a new major mode for NetLinx files called netlinx-mode
 ;; Use prog-mode as the parent mode (for programming languages)
@@ -147,7 +299,12 @@
 
     ;; Setup font-lock
     (setq-local treesit-font-lock-settings netlinx-mode--font-lock-settings)
-    (setq-local treesit-font-lock-feature-list '((keyword string)))
+    (setq-local treesit-font-lock-level 4)
+    (setq-local treesit-font-lock-feature-list
+                '((comment)
+                  (keyword string preprocessor)
+                  (constant number type boolean)
+                  (function variable property operator bracket delimiter error)))
 
     ;; Enable tree-sitter
     (treesit-major-mode-setup)))
--- test/main.axs
+++ test/main.axs
@@ -1,10 +1,22 @@
 PROGRAM_NAME='test'
 
+(************ Example ****************)
+/*************************************/
+#include 'Example.axi'
+
 DEFINE_DEVICE
 
 dvPROJ		=	5001:1:0
 dvTP		=	10001:1:0
 
+DEFINE_TYPE
+struct _Person {
+    integer bar
+    char x[100]
+}
+
+DEFINE_VARIABLE
+volatile char pattern[100] = '/foo/'
 
 DEFINE_EVENT
 
@@ -13,9 +25,27 @@ button_event[dvTP, 1] {
         send_string dvPROJ, "$02, 'ADZZ;PON', $03"
     }
 }
+#IF_DEFINED BAR
 
 button_event[dvTP, 2] {
     push: {
+        stack_var _Person person
+        person.bar = 1;
+
         send_string dvPROJ, "$02, 'ADZZ;POF', $03"
     }
 }
+
+define_function Foo() {
+    stack_var integer bar
+
+    bar = 1
+    bar = true
+    
+    for (x = 1; x <= 100; x++) {
+        x = find_string()
+    }
+    foo = mid_string(buf, 1, 2)
+}
+#END_IF
+// foo 
