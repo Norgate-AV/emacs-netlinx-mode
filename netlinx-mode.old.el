40b58a315f87129afaae9428ea184cd0d145ddf3
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2025-11-24 13:30:31 +0000

    chore: remove redundant code

--- netlinx-mode.old.el
+++ netlinx-mode.old.el
@@ -17,47 +17,10 @@
 
 ;;; Code:
 
-(require 'treesit)
-
-;; Ensure font-lock file can be loaded
-(eval-and-compile
-  (let ((dir (file-name-directory (or load-file-name buffer-file-name))))
-    (add-to-list 'load-path dir)))
-
-(require 'netlinx-mode-font-lock)
-
-(declare-function treesit-parser-create "treesit.c")
-(declare-function treesit-node-type "treesit.c")
-
-(defgroup netlinx nil
-  "Major mode for NetLinx using tree-sitter."
-  :group 'languages
-  :prefix "netlinx-")
-
-(defcustom netlinx-mode-grammar-location
-  "https://github.com/Norgate-AV/tree-sitter-netlinx"
-  "Repository URL for the tree-sitter NetLinx grammar."
-  :type 'string
-  :group 'netlinx)
-
-;; Add grammar source on load
-(add-to-list 'treesit-language-source-alist
-             '(netlinx "https://github.com/Norgate-AV/tree-sitter-netlinx"))
-
-(defun netlinx-mode--ensure-grammar ()
-  "Ensure NetLinx grammar is installed, installing if necessary."
-  (unless (treesit-language-available-p 'netlinx)
-    (message "NetLinx: Installing tree-sitter grammar (this may take a minute)...")
-    (treesit-install-language-grammar 'netlinx)
-    (message "NetLinx: Grammar installation complete")))
-
 ;;;###autoload
 (define-derived-mode netlinx-mode prog-mode "NetLinx"
   "Major mode for editing NetLinx files with tree-sitter support."
   :group 'netlinx
-
-  ;; Install grammar if needed
-  (netlinx-mode--ensure-grammar)
 
   (if (treesit-ready-p 'netlinx t)
       (progn
@@ -75,13 +38,5 @@
         ;; Enable tree-sitter
         (treesit-major-mode-setup))
     (message "NetLinx: tree-sitter grammar not available. Check *Messages* for errors.")))
-
-;;;###autoload
-(add-to-list 'auto-mode-alist '("\\.axs\\'" . netlinx-mode))
-
-;;;###autoload
-(add-to-list 'auto-mode-alist '("\\.axi\\'" . netlinx-mode))
-
-(provide 'netlinx-mode)
 
 ;;; netlinx-mode.el ends here
